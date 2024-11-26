2fe81b33d1bf49fb3de0337f3a3f7b47e07218b8
Ezequiel Blanco Mercado <ezeblamer@outlook.es>
2024-11-26 11:22:26 -0500

    PR feature/interact-blog correctedd

--- app/components/travel-card.js
+++ app/components/travel-card.js
@@ -1,6 +1,6 @@
 import Image from "next/image"
 
-export default function TravelCard({ id, imageUrl, priceColor, fill, price, title, abstract, authorUrl, authorBlog, published}) {
+export default function TravelCard({ id, imageUrl, priceColor, price, title, abstract, authorUrl, authorBlog, published}) {
   return (
     <div className="rounded-lg bg-white shadow-sm transition-shadow hover:shadow-md relative">
       <a href={`/posts/${id}`} className="absolute inset-0" aria-label="read the post content"></a>
@@ -28,8 +28,8 @@ export default function TravelCard({ id, imageUrl, priceColor, fill, price, titl
             {abstract}
           </dd>
         </dl>
-        <button type="button" className="block ml-auto mb-2 group" aria-label="add favorite">
-          <svg className="text-intenseGray group-hover:text-intenseYellow transition-colors duration-300" width="16" height="16" viewBox="0 0 16 16" fill={fill} xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <button type="button" className="block ml-auto mb-2 group relative" aria-label="add favorite">
+          <svg className="text-intenseGray group-hover:text-intenseYellow transition-colors duration-300" width="16" height="16" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
             <path fillRule="evenodd" clipRule="evenodd" d="M16 6.20038C16 6.38185 15.875 6.55325 15.75 6.68431L12.2596 10.2533L13.0865 15.2943C13.0962 15.3648 13.0962 15.4253 13.0962 15.4959C13.0962 15.758 12.9808 16 12.7019 16C12.5673 16 12.4327 15.9496 12.3173 15.879L8 13.4997L3.68269 15.879C3.55769 15.9496 3.43269 16 3.29808 16C3.01923 16 2.89423 15.758 2.89423 15.4959C2.89423 15.4253 2.90385 15.3648 2.91346 15.2943L3.74038 10.2533L0.240385 6.68431C0.125 6.55325 0 6.38185 0 6.20038C0 5.89792 0.298077 5.77694 0.538462 5.73661L5.36538 5.00063L7.52885 0.413359C7.61538 0.221802 7.77885 0 8 0C8.22115 0 8.38462 0.221802 8.47115 0.413359L10.6346 5.00063L15.4615 5.73661C15.6923 5.77694 16 5.89792 16 6.20038Z" />
           </svg>
         </button>
